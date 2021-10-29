dd4a145f13c29d9ba2ded7e901f963f1be20e39d
vic <vichsu38@gmail.com>
2021-10-29 20:30:29 +0800

    spring

--- SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Employee.java
+++ SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Employee.java
@@ -1,5 +1,81 @@
 package com.example.demo.entity;
 
-public class Employee {
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@OneToMany(mappedBy = "employee")
+	@OrderBy("id ASC") //尋找照ID有序
+    private Set<Order> orders = new LinkedHashSet<>();
+	
+	@OneToMany(mappedBy = "employee")
+	@OrderBy("id ASC") //尋找照ID有序
+    private Set<Purchase> purchases = new LinkedHashSet<>();
+	
+	@JoinColumn(name = "department_id") //生成關聯欄位(外鍵)
+	@ManyToOne
+	private Department department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Set<Order> orders) {
+		this.orders = orders;
+	}
+
+	public Set<Purchase> getPurchases() {
+		return purchases;
+	}
+
+	public void setPurchases(Set<Purchase> purchases) {
+		this.purchases = purchases;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
 }
--- SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Order.java
+++ SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Order.java
@@ -1,5 +1,84 @@
 package com.example.demo.entity;
 
-public class Order {
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	
+	@OneToMany(mappedBy = "order")
+	@OrderBy("id ASC") //尋找照ID有序
+    private Set<OrderItem> orderItems = new LinkedHashSet<>();
+	
+	@JoinColumn(name = "customer_id") //生成關聯欄位(外鍵)
+	@ManyToOne
+	private Customer customer;
+	
+	@JoinColumn(name = "employee_id") //生成關聯欄位(外鍵)
+	@ManyToOne
+	private Employee employee;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Set<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(Set<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	
 }
