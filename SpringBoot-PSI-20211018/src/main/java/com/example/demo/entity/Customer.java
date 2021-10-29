12b1768b59ce52cc3a8a99dbcf2c5f86066ad8de
vic <vichsu38@gmail.com>
2021-10-29 20:15:39 +0800

    spring

--- SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Customer.java
+++ SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Customer.java
@@ -1,5 +1,50 @@
 package com.example.demo.entity;
 
-public class Customer {
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@OneToMany(mappedBy = "customer")
+	@OrderBy("id ASC") //尋找照ID有序
+    private Set<Order> orders = new LinkedHashSet<>();
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Set<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(Set<Order> orders) {
+		this.orders = orders;
+	} 	
+	
+	
+	
 }
--- SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Product.java
+++ SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Product.java
@@ -1,5 +1,89 @@
 package com.example.demo.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String name;
+	
+	@Column
+	private Integer cost;
+	
+	@Column
+	private Integer price;
+	
+	@OneToMany(mappedBy = "product")
+	@OrderBy("id ASC") //尋找照ID有序
+    private Set<OrderItem> orderItems = new LinkedHashSet<>();
+	
+	@OneToMany(mappedBy = "product")
+	@OrderBy("id ASC") //尋找照ID有序
+    private Set<PurchaseItem> purchaseItems = new LinkedHashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCost() {
+		return cost;
+	}
+
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Set<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(Set<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	public Set<PurchaseItem> getPurchaseItems() {
+		return purchaseItems;
+	}
+
+	public void setPurchaseItems(Set<PurchaseItem> purchaseItems) {
+		this.purchaseItems = purchaseItems;
+	}
+
+	
 }
