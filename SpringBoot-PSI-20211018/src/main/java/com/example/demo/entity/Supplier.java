cb39c92c218ce0fb3dac06fed1cbd3387443ccfb
vic <vichsu38@gmail.com>
2021-10-29 20:17:15 +0800

    spring

--- SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Supplier.java
+++ SpringBoot-PSI-20211018/src/main/java/com/example/demo/entity/Supplier.java
@@ -1,5 +1,56 @@
 package com.example.demo.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "supplier")
 public class Supplier {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String name;
+	
+	@OneToMany(mappedBy = "suppliers")
+	@OrderBy("id ASC") //尋找照ID有序
+    private Set<Purchase> purchases = new LinkedHashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Purchase> getPurchases() {
+		return purchases;
+	}
+
+	public void setPurchases(Set<Purchase> purchases) {
+		this.purchases = purchases;
+	}
+	
+	
 
 }
