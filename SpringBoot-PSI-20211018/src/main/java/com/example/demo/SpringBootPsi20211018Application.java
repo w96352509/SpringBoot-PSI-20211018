69d4770fbf5753609cbfb90d582ced214901acbd
vic <vichsu38@gmail.com>
2021-10-29 19:43:42 +0800

    spring

---
+++ SpringBoot-PSI-20211018/src/main/java/com/example/demo/SpringBootPsi20211018Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPsi20211018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPsi20211018Application.class, args);
+	}
+
+}
